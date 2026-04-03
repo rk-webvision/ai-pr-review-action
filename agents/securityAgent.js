e6d76bebe68f69bca76ed0944a144563b4698f19
rk-webvision <128177080+rk-webvision@users.noreply.github.com>
2026-04-03 06:23:36 +0000

    merge conflicts

--- agents/securityAgent.js
+++ agents/securityAgent.js
@@ -1,21 +1,20 @@
 const OpenAI = require("openai");
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 async function securityAgent(diff) {
-  const res = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "user",
-        content: `Find security issues in this code:\n${diff}`,
-      },
-    ],
-  });
-  console.log("Security");
-  return res.choices[0].message.content;
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const res = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role: "user",
+                content: `Find security issues in this code:\n${diff}`,
+            },
+        ],
+    });
+
+    return res.choices[0].message.content;
 }
 
 module.exports = { securityAgent };
